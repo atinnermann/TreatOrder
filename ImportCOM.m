22950d5599e51b81acf595449ab9bb614c806675
ATinnermann <atinnermann85@gmail.com>
2023-08-11 11:15:13 +0200

    alternatives to find exp temps in case calibration failed plus test stimuli in the beginning

--- ImportCOM.m
+++ ImportCOM.m
@@ -2,27 +2,28 @@ function com = ImportCOM(hostname,thermoino)
 
 if thermoino == 1
     if strcmp(hostname,'stimpc1')
-        
+        addpath('');
         com.thermoPort    = 'COM11';
         com.thermoBaud    = 115200;
-        com.thermoino     = thermoino;
-        
+        com.thermoino     = thermoino;  
         com.CEDTrigger = 4; % Trigger bits for device
-        
-    else
+    elseif strcmp(hostname,'isn0068ebea3a78')   
+        addpath('C:\Users\alexandra\Documents\MATLAB\toolbox\Thermoino');
         com.thermoPort    = 'COM5';
         com.thermoBaud    = 115200;
         com.thermoino     = thermoino;
-        
         com.CEDTrigger = 255; % Trigger bits for thermode
-        
+    else
+        addpath('C:\Users\Mari Feldhaus\Documents\MATLAB\toolbox\Thermoino');
+        com.thermoPort    = 'COM5';
+        com.thermoBaud    = 115200;
+        com.thermoino     = thermoino; 
+        com.CEDTrigger = 255; % Trigger bits for thermode     
     end
-elseif thermoino == 2
-    
+elseif thermoino == 2 
     com.CEDport     = 255; % Trigger bits for thermode
     com.CEDaddress  = 888;
     com.CEDduration = 0.005;
-    
     addpath('C:\Users\Mari Feldhaus\Documents\MATLAB\IO_64bit');
     config_io;
     outp(com.CEDaddress,0);
